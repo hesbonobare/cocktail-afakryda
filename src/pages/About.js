d5bdfde4ee0a77217d53719ddb205049de7a6ee4
Hesbon Obare <hesbonobare@gmail.com>
2023-02-14 00:36:20 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rerum, quisquam veniam vero quam tempore voluptate perferendis quia iste, laudantium accusantium. Sapiente error molestiae 
+        molestias, neque praesentium dicta sequi sit culpa! Ea, vero numquam. Molestias eos consequuntur perspiciatis temporibus? Expedita!</p>
+    </section>
+  )
+}
+
+export default About
